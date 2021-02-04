879edb122467c4fc98125cf74f3d9c7b1bfbb3b4
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-02-03 20:53:28 -0800

    for...of loops

--- app.ts
+++ app.ts
@@ -1,14 +1,11 @@
 var container = document.getElementById("container");
 
-var todo = {
-    id: 123,
-    name: "Pick up dry cleaning",
-    completed: true
-}
+var array = [
+    "Do this",
+    "Do that",
+    "Do it all"
+];
 
-container.innerHTML = `
-<div todo='${todo.id}' class="list-group-item}">
-    <i class="${ todo.completed ? '' : 'hidden' } text-success glyphicon glyphicon-ok"></i>
-    <span class="name">${todo.name}</span>
-</div>
-`
+for(const value of array){
+    console.log(`${value}`);
+}
