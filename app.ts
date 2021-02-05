ecfdd5b0454d206da9e0b122277496084fb145a4
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-02-05 01:02:04 -0800

    custom types with interfaces

--- app.ts
+++ app.ts
@@ -1,40 +1,15 @@
-let array = [123, "Pick up dry cleaning", false];
-let [ id, title, completed] = array;
 
-console.log(String(id) + title + completed);
-
-let options = {
-    one: 20,
-    two: 4,
-    three: 2
+interface Todo {
+    name: string;
+    completed ?: boolean;
 }
 
-function getOptions (){
-    return {
-        one: 30,
-        two: 10,
-        three: 10
-    }
+interface ITodoService {
+    add(todo: Todo) : Todo;
+    delete(todoId: number) : void;
+    getAll() : Todo[];
+    getById(todoId : number) : Todo;
 }
 
-function countdown({one, two: final = 0,
-                       three: interval = 1, one: count}){
-
-    while(count > final){
-        count -= interval;
-        console.log(count);
-    }
-}
-
-countdown({one: 10});
-countdown(options);
-
-countdown(getOptions());
-
-function addValues(...values) {
-    let sum = 0;
-    for(let value of values){
-        sum += value;
-    }
-    return sum;
-}
+let todo : Todo = { name: "get measurements" };
+let todo2 : Todo = { name: "calculate body fat", completed: false };
