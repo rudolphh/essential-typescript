34cbed3427369de3849725310218a7eb48e81d64
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-02-07 22:53:32 -0800

    defining anonymous types

--- app.ts
+++ app.ts
@@ -1,22 +1,4 @@
-interface Todo {
-    name: string,
-    state: TodoState
-}
 
-var todo: Todo = {
-    name: "Pick up dry cleaning",
-    state: TodoState.New
-}
 
-enum TodoState {
-    New = 1,
-    Active,
-    Complete,
-    Delete
-}
-
-function Delete(todo: Todo) {
-    if (todo.state != TodoState.Complete) {
-        throw "Can't delete incomplete task!"
-    }
-}
+let todo : { name: string };
+todo = { age: 43 } // error
